a76d8e103e46f894d573a1c63a533fb4f19eb551
bienvllnv <bienvllnv111@gmail.com>
2021-01-23 00:13:48 +0800

    commit

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-var answer = $(this).click(function(){
-    $(this).addClass("show");
-});
 
 $("p").hide();
 
-$("div.number-1").click(function(){
-    $("p", this).addClass("answers")
-    answer();
-});
-
-function answer(){
-    $(".answers").show();
-}
